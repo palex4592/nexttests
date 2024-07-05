532f59c2cddc70d4e9279ace937b2ca8b43f3f75
palex4592 <palex459230@gmail.com>
2024-07-05 01:15:49 -0400

    search async

--- app/docs/search/page.tsx
+++ app/docs/search/page.tsx
@@ -1,5 +1,5 @@
 import Search from "@/app/ui/search";
-export default function Page() {
+export default async function Page() {
   return (
     <main className="flex text-4xl flex-col items-center justify-between p-24">
       <h1 className="p-24">Search Homepage</h1>
