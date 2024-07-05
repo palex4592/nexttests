ce379f78864b08ec29a8232510898539fbeb5e2d
palex4592 <palex459230@gmail.com>
2024-07-05 01:21:45 -0400

    rip search

--- app/docs/search/page.tsx
+++
@@ -1,9 +0,0 @@
-import Search from "@/app/ui/search";
-export default async function Page() {
-  return (
-    <main className="flex text-4xl flex-col items-center justify-between p-24">
-      <h1 className="p-24">Search Homepage</h1>
-      <Search placeholder={"search..."} />
-    </main>
-  );
-}
